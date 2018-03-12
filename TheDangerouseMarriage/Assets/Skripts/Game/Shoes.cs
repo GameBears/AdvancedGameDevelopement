24695eeac61628f9f238c683faa765b57deb44a0
Gabriel Moczalla <gmoczalla@yahoo.de>
2018-03-12 14:43:54 +0100

    Suhe verschmwinden im richtigen moment

---
+++ TheDangerouseMarriage/Assets/Skripts/Game/Shoes.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ShoeMode
+{
+    NOTVISIBLETILL,
+    VISIBLEFROM
+}
+
+public class Shoes : MonoBehaviour {
+    public ShoeMode mode;
+    public int day;
+    GameManagement gameManager;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = GameObject.Find("Background").GetComponent<GameManagement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (mode == ShoeMode.NOTVISIBLETILL)
+        {
+            if (gameManager.getDay() <= day)
+            {
+                GetComponent<SpriteRenderer>().sortingLayerName = "NotVisible";
+            }
+        }
+        else
+        {
+            if (gameManager.getDay() >= day)
+            {
+                GetComponent<SpriteRenderer>().sortingLayerName = "NotVisible";
+            }
+        }
+	}
+}
