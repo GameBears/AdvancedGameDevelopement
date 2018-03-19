701c49543eee92f40773877172abae5f237dd0a6
Gabriel Moczalla <gmoczalla@yahoo.de>
2018-03-19 12:27:24 +0100

    Kühlschrank und Herd

---
+++ TheDangerouseMarriage/Assets/Skripts/Game/Fridge.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fridge : MonoBehaviour {
+    public Sprite fridgeUsed;
+    Sprite fridgeUnused;
+    Actions fridgeAction;
+
+    // Use this for initialization
+    void Start()
+    {
+        fridgeUnused = GetComponent<SpriteRenderer>().sprite;
+        fridgeAction = GetComponent<Actions>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fridgeAction.done)
+        {
+            GetComponent<SpriteRenderer>().sprite = fridgeUsed;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = fridgeUnused;
+        }
+    }
+}
---
+++ TheDangerouseMarriage/Assets/Skripts/Game/Stove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stove : MonoBehaviour {
+    public Sprite stoveUsed;
+    Sprite stoveUnused;
+    Actions stoveAction;
+
+    // Use this for initialization
+    void Start()
+    {
+        stoveUnused = GetComponent<SpriteRenderer>().sprite;
+        stoveAction = GetComponent<Actions>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stoveAction.done)
+        {
+            GetComponent<SpriteRenderer>().sprite = stoveUsed;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = stoveUnused;
+        }
+    }
+}
