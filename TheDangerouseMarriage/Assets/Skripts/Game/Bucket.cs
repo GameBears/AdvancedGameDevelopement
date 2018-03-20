de0e5b7f1d27bc1970f59f2da84a6cd25525a9b1
Gabriel Moczalla <gmoczalla@yahoo.de>
2018-03-20 17:02:11 +0100

    Staubsauger und Eimer verschwinden nach Benutzung

---
+++ TheDangerouseMarriage/Assets/Skripts/Game/Bucket.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucket : MonoBehaviour {
+    Actions action;
+
+    // Use this for initialization
+    void Start()
+    {
+        action = GetComponent<Actions>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (action.done)
+        {
+            GetComponent<SpriteRenderer>().sortingLayerName = "NotVisible";
+        }
+    }
+}
---
+++ TheDangerouseMarriage/Assets/Skripts/Game/Vacuum.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vacuum : MonoBehaviour {
+    Actions action;
+
+	// Use this for initialization
+	void Start () {
+        action = GetComponent<Actions>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (action.done)
+        {
+            GetComponent<SpriteRenderer>().sortingLayerName = "NotVisible";
+        }
+	}
+}
